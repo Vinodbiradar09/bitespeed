24c683a2559ae9cc26ab999db0e52a2f50e73651
Vinodbiradar09 <vinodjb07@gmail.com>
2026-02-26 02:24:46 +0530

    prisma nd nestjs init

---
+++ src/contacts/contacts.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ContactsService } from './contacts.service';
+import { ContactsController } from './contacts.controller';
+import { PrismaServie } from 'src/prisma.service';
+
+@Module({
+  controllers: [ContactsController],
+  providers: [ContactsService, PrismaServie],
+})
+export class ContactsModule {}
---
+++ src/contacts/contacts.service.ts
@@ -0,0 +1,11 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateContactDto } from './dto/create-contact.dto';
+import { PrismaServie } from 'src/prisma.service';
+import { Contact, Prisma } from '@prisma/client';
+
+@Injectable()
+export class ContactsService {
+  constructor(private readonly db: PrismaServie) {}
+  async create(createContactDto: CreateContactDto) {
+   
+}
